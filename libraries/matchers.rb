2aeb31f94094b0f31218ac002eff91748a928201
Chengkai Liang <chengkai.liang@m-mbp.local>
2015-12-31 08:05:15 -0800

    return :s3_file symbol so we can write test cases for notifications

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,4 +1,7 @@
 if defined?(ChefSpec)
+  # this matcher allows to write a test case against notifications
+  ChefSpec::Runner.define_runner_method(:s3_file)
+
   def create_s3_file(path)
     ChefSpec::Matchers::ResourceMatcher.new(:s3_file, :create, path)
   end
